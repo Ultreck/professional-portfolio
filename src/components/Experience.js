e8efb3d05922e83704de53a16078acc714fda84e
Ultreck <adetutuemmanueloluwatayese@gmail.com>
2023-03-21 08:37:09 +0100

    proUpdate

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,11 +2,13 @@ import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
 // import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 import '@splidejs/react-splide/css';
 import Aos from 'aos';
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
-import Images from './Images';
+import Images from './Images'
+import Projects from './Projects';
 
 const Experience = () => {
+  const [infoState, setinfoState] = useState('hidden absolute self-center left-0 text-white lg:px-8 px-1 lg:left-20' )
   // Aos.init();
   const options = {
     type         : 'loop',
@@ -57,10 +59,26 @@ const Experience = () => {
       speed: 400,
   };
  
+
+
+  const handleClick = (e) =>{
+    console.log(e);
+  }
+  const handleMouseOver = () =>{
+    setinfoState("flex absolute self-center left-0 text-white lg:px-8 px-1 lg:left-20")
+  }
+  const hndleMouseOut = () =>{
+    setinfoState("hidden absolute self-center left-0 text-white lg:px-8 px-1 lg:left-20")
+  }
   return (
     <>
         <div className="text dark:bg-slate-800 h-screen bg-white">
           <div className="text">
+                      {/* <div className="text absolute w-full h-screen">
+                        <img src={
+
+                        } alt="" className="text" />
+                      </div> */}
             <div className="text" >
               <h1 className="text-center font-bold text-2xl pt-3 underline dark:text-orange-500 text-black">Skills/Tools</h1>
             </div>
@@ -98,29 +116,32 @@ const Experience = () => {
              </div>
              <Splide hasTrack={ false } options={options3} className=' hidden md:flex py-2'>
                 <SplideTrack>
-                {Images.map((val, index) => (
+                {Projects.map((val, index) => (
                   <SplideSlide>
                     <div class="flex justify-center">
                         <div
                          class="flex flex-col rounded-lg bg-white shadow-lg  dark:bg-slate-600 md:max-w-xl md:flex-row ">
-                        <img
-                        class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                       src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg"
+                        <img onMouseOut={hndleMouseOut} onMouseOver={handleMouseOver} onClick={(val) =>handleClick(val)} title='click to view full portrate'
+                        class="h-96 w-full rounded-t-lg object-cover hover:brightness-50 md:h-auto md:w-72 md:rounded-none md:rounded-l-lg"
+                        src={val.pic}
                       alt="" />
+                      <div className="text flex items-stretch text-start">
+                            <span className={infoState}>Click for full portrate</span>
+                      </div>
                       <div class="flex flex-col justify-start p-6">
                        <h5
                       class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
                      {val.title}
                     </h5>
                       <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                      This is a wider card with supporting text below as a natural
-                       lead-in to additional content. This content is a little bit
-                       longer.
+                      {val.description}
                      </p>
                     <p class="text-xs text-neutral-500 dark:text-neutral-300">
                          Last updated 3 mins ago
                     </p>
-                      <Link className='bg-blue-700 px-5 py-1 text-center mt-5  text-white' to="">Browse App</Link>
+                        <a href={`${val.path}`} className="text bg-blue-700 px-5 py-1 text-center mt-5  text-white">
+                            Browse App
+                        </a>
                  </div>
                  </div>
               </div>
@@ -133,15 +154,18 @@ const Experience = () => {
 
               <Splide hasTrack={ false } options={options4} className='flex  md:hidden py-2 '>
                 <SplideTrack>
-                {Images.map((val, index) => (
+                {Projects.map((val, index) => (
                   <SplideSlide>
                     <div class="flex justify-center">
                   <div
                     class="flex  rounded-lg bg-white shadow-lg dark:bg-slate-600 md:max-w-xl md:flex-row">
-                    <img
-                      class="h-full w-1/3 rounded-t-lg object-cover md:h-auto  md:rounded-none md:rounded-l-lg"
-                      src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg"
+                    <img onMouseOut={hndleMouseOut} onMouseOver={handleMouseOver} onClick={handleClick} title='click to view full portrate'
+                      class="h-full w-2/5 rounded-t-lg object-cover hover:brightness-50 md:h-auto  md:rounded-none md:rounded-l-lg"
+                      src={val.pic}
                       alt="" />
+                    <div className="text flex items-stretch  text-start">
+                            <span className={infoState}>Click for full portrate</span>
+                      </div>
                     <div class="flex flex-col justify-start p-6">
                       <h5
                         class="mb-2 text-xl font-medium text-neutral-800 dark:text-white">
@@ -155,7 +179,9 @@ const Experience = () => {
                       <p class="text-xs text-neutral-500 dark:text-neutral-300">
                         Last updated 3 mins ago
                       </p>
-                      <Link className='bg-blue-700 px-5 py-1 text-center mt-5  text-white' to="">Browse App</Link>
+                      <a href={`${val.path}`} className="text bg-blue-700 px-5 py-1 text-center mt-5  text-white">
+                            Browse App
+                        </a>
                     </div>
                   </div>
                 </div>
---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+
+const Projects =  [
+      {
+      pic:require('../images/Desktop.png'),
+       path:"https://medical-management-system.vercel.app", 
+      title:"React And Firebase", 
+      description:"A hospital management system, The system that shows all patients and available doctor's data and it's a well responsive and interractive web app."}
+]
+export default Projects
