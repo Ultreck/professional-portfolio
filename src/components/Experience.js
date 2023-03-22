4d57aee2c7e1659bdf51fffb4c7f3b2002a28bd7
Ultreck <adetutuemmanueloluwatayese@gmail.com>
2023-03-22 14:33:58 +0100

    proUpdate

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,7 +8,9 @@ import Images from './Images'
 import Projects from './Projects';
 
 const Experience = () => {
-  const [infoState, setinfoState] = useState('hidden absolute self-center left-0 text-white lg:px-8 px-1 lg:left-20' )
+  const [infoState, setinfoState] = useState('hidden absolute self-center left-0 text-white lg:px-8 px-1 lg:left-28' )
+  const [clickedPix, setclickedPix] = useState({})
+  const [showPix, setshowPix] = useState("hidden z-50  absolute w-full h-screen text-center  dark:bg-slate-500 bg-white pt-20")
   // Aos.init();
   const options = {
     type         : 'loop',
@@ -62,23 +64,34 @@ const Experience = () => {
 
 
   const handleClick = (e) =>{
-    console.log(e);
+    setclickedPix(e)
+      setshowPix("z-50  absolute w-full h-screen text-center dark:bg-slate-500 bg-white pt-20")
+    console.log(clickedPix);
   }
+  const handleHide = (e) =>{
+    setclickedPix(e)
+      setshowPix("hidden z-50  absolute w-full h-screen text-center  dark:bg-slate-500 bg-white pt-20")
+    console.log(clickedPix);
+  }
+
+
   const handleMouseOver = () =>{
-    setinfoState("flex absolute self-center left-0 text-white lg:px-8 px-1 lg:left-20")
+    setinfoState("flex absolute self-center left-0 text-white lg:px-8 px-1 lg:left-28")
   }
   const hndleMouseOut = () =>{
-    setinfoState("hidden absolute self-center left-0 text-white lg:px-8 px-1 lg:left-20")
+    setinfoState("hidden absolute self-center left-0 text-white lg:px-8 px-1 lg:left-28")
   }
+
+
+
   return (
     <>
         <div className="text dark:bg-slate-800 h-screen bg-white">
           <div className="text">
-                      {/* <div className="text absolute w-full h-screen">
-                        <img src={
-
-                        } alt="" className="text" />
-                      </div> */}
+                      <div className={showPix} onClick={handleHide}>
+                        <span className="text-2xl text-red-500 absolute right-20 border rounded-full w-9 border-red-600 bg-red-300 cursor-pointer">X</span>
+                        <img src={clickedPix.pic} alt="" className="text w-full lg:w-2/3 h-3/4 mx-auto py-10" />
+                      </div>
             <div className="text" >
               <h1 className="text-center font-bold text-2xl pt-3 underline dark:text-orange-500 text-black">Skills/Tools</h1>
             </div>
@@ -112,7 +125,7 @@ const Experience = () => {
           </div>
           <div className="text">
               <div className="text" >
-                  <h1 className="text-center font-bold text-2xl pt-5 underline dark:text-orange-500 text-black">Projects</h1>
+                  <h1 className="text-center font-bold text-2xl pt-2 underline dark:text-orange-500 text-black">Projects</h1>
              </div>
              <Splide hasTrack={ false } options={options3} className=' hidden md:flex py-2'>
                 <SplideTrack>
@@ -121,7 +134,7 @@ const Experience = () => {
                     <div class="flex justify-center">
                         <div
                          class="flex flex-col rounded-lg bg-white shadow-lg  dark:bg-slate-600 md:max-w-xl md:flex-row ">
-                        <img onMouseOut={hndleMouseOut} onMouseOver={handleMouseOver} onClick={(val) =>handleClick(val)} title='click to view full portrate'
+                        <img onMouseOut={hndleMouseOut} onMouseOver={handleMouseOver} onClick={() =>handleClick(val)} title='click to view full portrate'
                         class="h-96 w-full rounded-t-lg object-cover hover:brightness-50 md:h-auto md:w-72 md:rounded-none md:rounded-l-lg"
                         src={val.pic}
                       alt="" />
@@ -156,34 +169,31 @@ const Experience = () => {
                 <SplideTrack>
                 {Projects.map((val, index) => (
                   <SplideSlide>
-                    <div class="flex justify-center">
-                  <div
-                    class="flex  rounded-lg bg-white shadow-lg dark:bg-slate-600 md:max-w-xl md:flex-row">
-                    <img onMouseOut={hndleMouseOut} onMouseOver={handleMouseOver} onClick={handleClick} title='click to view full portrate'
-                      class="h-full w-2/5 rounded-t-lg object-cover hover:brightness-50 md:h-auto  md:rounded-none md:rounded-l-lg"
+                    <div class="gird pb-3 justify-center w-5/6 mx-auto bg-white shadow-lg dark:bg-slate-700  rounded-t-lg p-2">
+                      <div
+                    class="flex  rounded-t-lg bg-white shadow-lg dark:bg-slate-600 md:max-full h-32 md:flex-row ">
+                    <img onMouseOut={hndleMouseOut} onMouseOver={handleMouseOver} onClick={() =>handleClick(val)} title='click to view full portrate'
+                      class="h-full w-full rounded-t-lg object-cover hover:brightness-50 md:h-auto  md:rounded-none md:rounded-l-lg mx-auto "
                       src={val.pic}
                       alt="" />
                     <div className="text flex items-stretch  text-start">
                             <span className={infoState}>Click for full portrate</span>
-                      </div>
+                    </div>
+                    </div>
                     <div class="flex flex-col justify-start p-6">
                       <h5
-                        class="mb-2 text-xl font-medium text-neutral-800 dark:text-white">
+                        class="mb-2 text-md font-medium text-neutral-800 dark:text-white">
                         {val.title}
                       </h5>
-                      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                      <p class="mb-4 text-sm text-neutral-600 dark:text-neutral-200">
                         This is a wider card with supporting text below as a natural
                         lead-in to additional content. This content is a little bit
                         longer.
                       </p>
-                      <p class="text-xs text-neutral-500 dark:text-neutral-300">
-                        Last updated 3 mins ago
-                      </p>
-                      <a href={`${val.path}`} className="text bg-blue-700 px-5 py-1 text-center mt-5  text-white">
+                      <a href={`${val.path}`} className="text bg-blue-700 px-5 py-1 text-center  text-white">
                             Browse App
                         </a>
                     </div>
-                  </div>
                 </div>
                   </SplideSlide>
             ))}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 
 const Projects =  [
       {
-      pic:require('../images/Desktop.png'),
+      pic:require('../images/Desktop-removebg-preview.png'),
        path:"https://medical-management-system.vercel.app", 
       title:"React And Firebase", 
       description:"A hospital management system, The system that shows all patients and available doctor's data and it's a well responsive and interractive web app."}
